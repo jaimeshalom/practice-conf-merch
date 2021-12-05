e18c217044e300c0a1ae4e3842814303d23bb4d1
jaimeshalom <jaimeshalom12@gmail.com>
2021-12-05 18:03:47 -0400

    add basic react-helmet

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import Products from '../components/Products';
 import initialState from '../initialState';
 
 const Home = () => {
-  return <Products products={initialState.products} />;
+  return (
+    <>
+      <Helmet>
+        <title>Platzi Conf Merch Productos</title>
+      </Helmet>
+      <Products products={initialState.products} />
+    </>
+  );
 };
 
 export default Home;
